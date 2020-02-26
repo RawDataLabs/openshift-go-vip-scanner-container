0ea107decf594ba1f7eb354fc4b40c3b00c3a0d9
Alin Spinu <spinualin@gmail.com>
2020-02-26 16:13:08 +0200

    prepare for K8s Informer Controller

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/rawc0der/VIP_Scanner
 
 go 1.12
+
+require (
+	github.com/garyburd/redigo v1.6.0
+	github.com/gomodule/redigo v2.0.0+incompatible // indirect
+	k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
+	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
+)
--- main.go
+++ main.go
@@ -1,21 +1,173 @@
 package main
 
-
 import (
 	"fmt"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	// "log"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/tools/clientcmd"
-	"github.com/gorilla/mux"
+
+	"github.com/gomodule/redigo/redis"
+
 )
 
+type Image struct {
+	podName string
+	image string
+	scanned bool
+	vulnerabilities []string
+}
+
+
 func main() {
-	// uses the current context in kubeconfig
-	// path-to-kubeconfig -- for example, /root/.kube/config
-	config, _ := clientcmd.BuildConfigFromFlags("", "kubeconfig_local_k8s")
-	// creates the clientset
-	clientset, _ := kubernetes.NewForConfig(config)
-	// access the API to list pods
-	pods, _ := clientset.CoreV1().Pods("").List(v1.ListOptions{})
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	redisConn := *RedisStart()
+
+	c := make(chan Image)
+
+	go ListKubePods(c)
+
+	for rx := range c {
+		fmt.Println("Recieving:")
+		fmt.Println(rx)
+		addToRedisHash(redisConn, rx)
+		//publishNewImageScanEvent(rx)
+	}
+
+}
+
+func getPodsFromRedis (redisConn redis.Conn) {
+	//  Get pods from redis
+	fmt.Println("Searching for Pods in REDIS:")
+	values, err := redis.Values(redisConn.Do("KEYS", "pod:*"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for len(values) > 0 {
+		var pod string
+		values, err = redis.Scan(values, &pod)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("found pod: "+pod)
+		getRedisHashStr(redisConn, pod)
+
+	}
+}
+
+func getRedisHashStr(redisConn redis.Conn, podName string) {
+	image, err := redis.String(redisConn.Do("HGET", podName, "image"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%+v\n", image)
+}
+
+func getRedisHash(redisConn redis.Conn, podName string) {
+	values, err := redis.Values(redisConn.Do("HGET", podName, "image"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for len(values) > 0 {
+		var image string
+		values, err = redis.Scan(values, &image)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("found Pod.imgage: "+image)
+
+	}
+}
+
+func addToRedisHash(redisConn redis.Conn, rx Image) {
+	redisCmd := fmt.Sprintf("HSET pod:%s image %s", rx.podName, rx.image  )
+	fmt.Println(redisCmd)
+
+	//@todo
+	//redisConn.Do("HMSET", redis.Args{"pod:"+rx.podName}.AddFlat(rx)...)
+	//_, err := redisConn.Do("HMSET", redis.Args{}.Add("pod:"+rx.podName).AddFlat(&rx)...)
+
+	redisConn.Do("LPUSH", "pods", rx.podName)
+	_, err := redisConn.Do("HMSET", "pod:"+rx.podName, "image", rx.image, "scanned", rx.scanned, "vulnerabilities", rx.vulnerabilities)
+	if err != nil {
+		return
+	}
+
+
+}
+
+func RedisStart() (conn *redis.Conn) {
+	c, err := redis.Dial("tcp", "a7e2fc632297911ea973806f8086ebf8-23313424.ca-central-1.elb.amazonaws.com:6379")
+	if err != nil {
+		panic(err)
+	}
+	_, err = c.Do("AUTH", "P4mayz4dAK")
+	if err != nil {
+		panic(err)
+	}
+	_, err2 := c.Do("SELECT", "2")
+	if err2 != nil {
+		panic(err)
+	}
+
+	name, err := redis.String(c.Do("GET", "name"))
+	if err != nil {
+		fmt.Println("name not found")
+	} else {
+		//Print our key if it exists
+		fmt.Println("name exists: " + name)
+	}
+	//defer c.Close()
+	return &c
+}
+
+ func  ListKubePods(c chan Image ) {
+	// Instantiate loader for kubeconfig file.
+	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		clientcmd.NewDefaultClientConfigLoadingRules(),
+		&clientcmd.ConfigOverrides{},
+	)
+
+	// Determine the Namespace referenced by the current context in the
+	// kubeconfig file.
+	namespace, _, err := kubeconfig.Namespace()
+	if err != nil {
+		panic(err)
+	}
+
+	// Get a rest.Config from the kubeconfig file.  This will be passed into all
+	// the client objects we create.
+	restconfig, err := kubeconfig.ClientConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a Kubernetes core/v1 client.
+	coreclient, err := corev1client.NewForConfig(restconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	// List all Pods in our current Namespace.
+	pods, err := coreclient.Pods(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Pods in namespace %s:\n", namespace)
+	for _, pod := range pods.Items {
+		// fmt.Printf("  %s\n", pod.Name)
+		//fmt.Printf("  %+v\n", pod)
+		//fmt.Println(pod.Name, pod.Spec.Containers[0].Image)
+		c <- Image{pod.Name ,  pod.Spec.Containers[0].Image, false, []string{} }
+	}
+	close(c)
+
 }
